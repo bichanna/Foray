f387ebe50314312eb5d8d1f6f3a9ce7da5a67b28
bichanna <nobu.bichanna@gmail.com>
2022-01-04 14:11:02 -0600

    Build very incomplete and shitty parser

--- src/Querulous.jl
+++ src/Querulous.jl
@@ -1,8 +1,15 @@
 include("./lexer.jl")
 using .Lexer
 
+include("./parser.jl")
+using .Parser
+
 filename = "test.qr"
 
 tokens = tokenize(filename)
 println("TOKENS:")
 println(tokens)
+
+AST = build_AST(tokens)
+println("AST:")
+println(AST)
---
+++ src/parser.jl
@@ -0,0 +1,54 @@
+module Parser
+
+AST = []
+
+function add_node(parent::Dict{Any, Any}, node::Dict{Any, Any})
+	for a in AST
+		if issubset(parent, a)
+			push!(a[parent], node)
+		end
+	end
+end
+
+function build_AST(tokens::Vector{Any})
+	saved = Dict()
+	parent = Dict()
+	collect = false
+
+	for token in tokens
+		if token["id"] == "label"
+			t = Dict(token["value"]=>[])
+			if parent != t
+				parent = token["value"]
+				push!(AST, t)
+			end
+		elseif token["id"] == "keyword"
+			if token["value"] == "end"
+				t = Dict(token["value"]=>0)
+				add_node(parent, t)
+			else
+				if collect == false
+					saved = token
+					collect = true
+				else
+					t = Dict(saved["value"]=>token["value"])
+					add_node(parent, t)
+					collect = false
+				end
+			end
+		elseif token["id"] == "char"
+			if collect == false
+				saved = token
+				collect = true
+			else
+				t = Dict(saved["value"]=>token["value"])
+				add_node(parent, t)
+				collect = false
+			end
+		end
+	end
+	return AST
+end
+
+export build_AST
+end # module
