117c613d0832a751f0d64253e56213b50045141e
bichanna <nobu.bichanna@gmail.com>
2022-01-06 16:24:46 -0600

    Get file name from command arguments

--- src/Querulous.jl
+++ src/Querulous.jl
@@ -7,19 +7,20 @@ using .Parser
 include("./evaluator.jl")
 using .Evaluator
 
-filename = "test.qr"
+filename = ARGS[1]
 
 tokens = tokenize(filename)
-println("TOKENS:")
-for t in tokens
-	println("""	  $(t["id"]): $(t["value"])""")
-end
+# println("TOKENS:")
+# for t in tokens
+# 	println("""	  $(t["id"]): $(t["value"])""")
+# end
 
 AST = build_AST(tokens)
-println("AST:")
-for a in AST
-	println(a)
-end
+# println("AST:")
+# println(AST)
+# for a in AST
+# 	println(a)
+# end
 
-println("OUTPUTS:")
+println("\nOUTPUTS:")
 evaluate(AST)
